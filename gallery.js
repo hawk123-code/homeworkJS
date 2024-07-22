04fa8bc60cca91ea8df5b1c2696b0c7e3fcac21f
hawk123-code <abdullahsallehaqeel123@gmail.com>
2024-07-22 15:44:08 +0500

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,14 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+    const x=document.getElementById('image');
+    x.style.backgroundImage=`url(${previewPic.src})`;
+    x.style.backgroundColor='#b3b3b3';
+    x.innerHTML=previewPic.alt;
+       }
+       function unDo(){
+        const x=document.getElementById('image');
+        x.style.backgroundImage="url('')";
+        x.style.backgroundColor='#8e68ff';
+        x.innerHTML="Hover over an image below to display here.";
+       }
